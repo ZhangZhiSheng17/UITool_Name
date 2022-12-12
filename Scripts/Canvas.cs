843b68d1f2a33ee926175e8f108c9dc0b6758e99
张老板 <119479640+ZhangZhiSheng17@users.noreply.github.com>
2022-12-12 10:10:38 +0800

    Add files via upload

---
+++ Scripts/Canvas.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+public class Canvas : MonoBehaviour
+{
+//auto
+    private Text Txt = null;
+	
+
+   public void Start()
+	{
+		Txt = gameObject.transform.Find("/Canvas/Txt").GetComponent<Text>();
+		
+	}
+}
